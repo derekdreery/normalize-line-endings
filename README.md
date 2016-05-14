8fbba12327a163c2feea8dabd3982b3de10d56c6
Richard Dodd <richard.o.dodd@gmail.com>
2016-05-14 20:02:26 +0100

    First real commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Normalize-line-endings
+
+Consume an `Iterator<Item=char>` and return another with normalized line
+endings
+
+[documentation](https://derekdreery.github.io/normailze-line-endings/index.php)
---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+#![warn(missing_docs)]
+//!
+//! Normalize line endings
+//!
+//! This crate provides a `normalize` method that takes a char iterator and returns
+//! a new one with `\n` for all line endings
+
+/// This struct wraps a `std::io::Chars` to normalize line endings.
+///
+/// Implements `Iterator<Item=char>` so can be used in place
+pub struct Normalized<I>
+where I: Iterator<Item=char> {
+    iter: I,
+    prev_was_cr: bool
+}
+
+/// Take a Chars and return similar struct with normalized line endings
+///
+/// # Example
+/// ```
+/// use std::iter::FromIterator;
+/// use normalize_line_endings::normalized;
+///
+/// let input = "This is a string \n with \r some \n\r\n random newlines\r\r\n\n";
+/// assert_eq!(
+///     &String::from_iter(normalized(input.chars())),
+///     "This is a string \n with \n some \n\n random newlines\n\n\n"
+/// );
+/// ```
+pub fn normalized<I>(iter: I) -> Normalized<I>
+where I: Iterator<Item=char> {
+    Normalized { iter: iter, prev_was_cr: false }
+}
+
+impl<I: Iterator<Item=char>> Iterator for Normalized<I> {
+    type Item = char;
+    fn next(&mut self) -> Option<char> {
+        match self.iter.next() {
+            Some('\n') if self.prev_was_cr => {
+                self.prev_was_cr = false;
+                match self.iter.next() {
+                    Some('\r') => {
+                        self.prev_was_cr = true;
+                        Some('\n')
+                    },
+                    any => {
+                        self.prev_was_cr = false;
+                        any
+                    }
+                }
+            },
+            Some('\r') => {
+                self.prev_was_cr = true;
+                Some('\n')
+            },
+            any => {
+                self.prev_was_cr = false;
+                any
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::prelude::*;
+    use super::normalized;
+    use std::iter::FromIterator;
+
+    #[test]
+    fn normalize() {
+        let input = "This is a string \n with \r some \n\r\n random newlines\r\r\n\n";
+        assert_eq!(
+            &String::from_iter(normalized(input.chars())),
+            "This is a string \n with \n some \n\n random newlines\n\n\n"
+        );
+    }
+}
